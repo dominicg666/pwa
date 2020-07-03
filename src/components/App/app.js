529efb79c32fc0345d9c1bbe9aee021bffc5ee4b
Dominic <dominic.webandcrafts@gmail.com>
2020-07-03 09:20:36 +0530

    update buikit

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -1,6 +1,6 @@
 import React, { useCallback, useEffect, Suspense, lazy } from 'react';
 import { array, func, shape, string } from 'prop-types';
-import { useApp } from '@baaz/adapter/talons/App/useApp';
+import { useApp } from '@baaz/adapter/lib/App/useApp';
 import { HeadProvider, Title } from 'buikit/lib/Head';
 import { registerMessageHandler } from 'buikit/util/swUtils';
 import Mask from 'buikit/lib/Mask';
